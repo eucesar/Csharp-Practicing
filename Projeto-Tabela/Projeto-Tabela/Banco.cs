3924a6159f2633568b240c1f7a863a1f36f8ce8c
Cesar Iglesias <cesaribneto.job@gmail.com>
2023-04-03 21:29:13 -0300

    projeto-tabela e exercicio

---
+++ Projeto-Tabela/Projeto-Tabela/Banco.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Tabela
+{
+    public class Banco
+    {
+        //front 
+        //posso melhorar com tempo, mas é apenas para revisar estrutura esse projeto.
+
+        //var
+        public string Nome { get; set; }
+        public double CPF { get; set; }
+        public string Profissao { get; set; }
+        public double Saldo { get; set; }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-Vindo ao Banco");
+
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine();
+            // Para atribuir o valor à propriedade "Nome", você precisa criar um objeto da classe "Banco" primeiro - preciosa ter algo na frente:
+            Banco banco = new Banco();
+            banco.Nome = nome;
+
+            Console.Write("Digite o CPF: ");
+            double cpf = double.Parse(Console.ReadLine());
+            banco.CPF = cpf;
+
+            Console.Write("Digite a profissão: ");
+            string profissao = Console.ReadLine();
+            banco.Profissao = profissao;
+
+            Console.Write("Digite o saldo: ");
+            double Saldo = double.Parse(Console.ReadLine());
+            banco.Saldo = Saldo;
+
+
+            Console.WriteLine("Obrigado voce acabou de criar uma conta no Banco");
+            Console.WriteLine("Pressione qualquer tecla para encerrar...");
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+    |\__/,|   (`\
+  _.|o o  |_   ) )
+-(((---(((-------- 𝒞𝑒𝓈𝒶𝓇 ℐ𝑔𝓁𝑒𝓈𝒾𝒶𝓈  */
