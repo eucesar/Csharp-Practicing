a21bcb183c7287974e62fc053440adc95c832f0e
Cesar Iglesias <cesaribneto.job@gmail.com>
2023-04-03 21:39:34 -0300

    ex.

---
+++ Exercicio/exercicio5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //var
+        string nome;
+        int quantidadeDeLetras;
+
+        //numero
+        Console.WriteLine("Digite uma string:");
+        nome = Console.ReadLine().ToLower();
+
+        //conta
+        quantidadeDeLetras = 0;
+
+        //saida
+        foreach (char quantidadeDeLtras in nome)
+        {
+            if ("aeiou".IndexOf(quantidadeDeLtras) >= 0)
+            {
+                quantidadeDeLetras++;
+            }
+        }
+
+        Console.WriteLine($"A string possui {quantidadeDeLetras} vogais.");
+    }
+}
+/*
+    |\__/,|   (`\
+  _.|o o  |_   ) )
+-(((---(((-------- 𝒞𝑒𝓈𝒶𝓇 ℐ𝑔𝓁𝑒𝓈𝒾𝒶𝓈  */
