fec2a1658fd63111a7b51211e4a0c0cd0a334a07
Cesar Iglesias <cesaribneto.job@gmail.com>
2023-04-03 21:36:12 -0300

    .

---
+++ Exercicio/exercicio6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        //var
+        int numeroLista1, numeroLista2;
+        bool iguais;
+
+        //numero
+        Console.Write("Digite um numero: ");
+        numeroLista1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite um numero: ");
+        numeroLista2 = int.Parse(Console.ReadLine());
+
+        //contas
+        iguais = numeroLista1 == numeroLista2;
+
+        
+        //saida
+        if (numeroLista1 > numeroLista2)
+        {
+            Console.WriteLine($"o {numeroLista1} é maior que o {numeroLista2}");
+        }
+        else
+        {
+            Console.WriteLine($"o {numeroLista2} é maior que o {numeroLista1}");
+        }
+
+        if (iguais) 
+        {
+            Console.WriteLine("obs.: os numeros da lista são iguais, troque os numeros para executar de uma melhor maneira");
+        }
+        
+        Console.ReadLine();
+    }
+}
+
+/*
+    |\__/,|   (`\
+  _.|o o  |_   ) )
+-(((---(((-------- 𝒞𝑒𝓈𝒶𝓇 ℐ𝑔𝓁𝑒𝓈𝒾𝒶𝓈  */
